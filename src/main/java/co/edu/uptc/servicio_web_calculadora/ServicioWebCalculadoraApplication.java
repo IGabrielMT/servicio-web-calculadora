797c9f2fd6a22a002a6ea8061a4fc7e77d3c3b52
Darpi <iangmartinezt@gmail.com>
2026-09-15 11:32:03 -0500

    Enabled Async

--- src/main/java/co/edu/uptc/servicio_web_calculadora/ServicioWebCalculadoraApplication.java
+++ src/main/java/co/edu/uptc/servicio_web_calculadora/ServicioWebCalculadoraApplication.java
@@ -2,8 +2,10 @@ package co.edu.uptc.servicio_web_calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ServicioWebCalculadoraApplication {
 
 	public static void main(String[] args) {
